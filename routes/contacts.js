b8adb08207df6044ce4e256a1185c7709e8b1ad0
olamideluc <bamigbolaluc@gmail.com>
2026-09-26 16:19:46 +0100

    Add REST client testing file

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,4 +8,13 @@ router.get('/', contactsController.getAll);
 // GET single contact by ID
 router.get('/:id', contactsController.getSingle);
 
+// POST new contact
+router.post('/', contactsController.createContact);
+
+// PUT update contact
+router.put('/:id', contactsController.updateContact);
+
+// DELETE contact
+router.delete('/:id', contactsController.deleteContact);
+
 module.exports = router;
